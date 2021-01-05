b43787efa745c8f40348943ad969a133e05a9b83
Mayank Vats <mv@rapidinnovation.dev>
2021-01-05 13:10:23 +0530

    initial commit

---
+++ src/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  
+  "1": {
+    "name": "Royale Rarities - Queen of Queens (Origins Series)",
+    "description": "Origins Series (#001 of 36)",
+    "image": "https://s3.amazonaws.com/royale.finance/queen/royaleNFT-queen01.jpg",
+    "external_url": "",
+    "attributes": [
+       ]
+  }
+  
+}
